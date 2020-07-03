3107cdf36dc3c3efaf88221dc285fc6ca3e55541
singhvikash630 <singh.vikash630@gmail.com>
2020-07-03 11:57:10 +0530

    code added for rest with angularjs in springboot

---
+++ springboot_with_angular/src/main/java/com/eureka/mvc/SpringbootWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithAngularApplication.class, args);
+	}
+
+}
---
+++ springboot_with_angular/src/test/java/com/eureka/mvc/SpringbootWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
